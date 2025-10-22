a42fbdd300d345564094543e9200ea8156e8f6b6
Aaron Perillat <barview@gmail.com>
2025-10-22 16:56:33 -0700

    UI glass and text are accurate

--- DRINK MORE WATER/ContentView.swift
+++ DRINK MORE WATER/ContentView.swift
@@ -7,17 +7,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let glassVerticalNudge: CGFloat = -6
+    private let textScale: CGFloat = 0.54
+    private let textOffset: CGSize = CGSize(width: 0, height: 58)
+
     var body: some View {
         ZStack {
             Color(.systemBlue)
-                .ignoresSafeArea()
 
-            Image("empty_glass")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 551, height: 722)
-                .accessibilityHidden(true)
+            ZStack {
+                Image("glass_text")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 551, height: 722)
+                    .scaleEffect(textScale)
+                    .offset(y: glassVerticalNudge)
+                    .offset(x: textOffset.width, y: textOffset.height)
+                    .accessibilityHidden(true)
+
+                Image("empty_glass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 551, height: 722)
+                    .offset(y: glassVerticalNudge)
+                    .accessibilityHidden(true)
+            }
         }
+        .ignoresSafeArea()
     }
 }
 
